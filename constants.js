87079c6afd11c498d58debc0d1021a34d2d58206
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-02-14 07:08:32 +0700

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,23 @@
+const bodyBackColor = "#fff";
+const bodyTextColor = "#fff";
+const bodyFont = "15px Segoe UI";
+const canvasBackColor = "#fff";
+const canvasPosition = "fixed";
+const audioVisibility = "hidden";
+const msgTextColor = "#000";
+const msgFontSize = 15;
+const msgFont = msgFontSize + "px Consolas";
+
+const interval = 10;
+
+
+const squareWidth = 40;
+const squareHeight = 40;
+const squareColor = "#f4a460";
+const remainColor = "#a4ff80";
+
+const trialTime = 150;
+const bound = 49;					// Must be sqrt-able, for example: 9, 16, 25, 36, 49, 64, 81, 100, 121, ...
+const startSquares = 3;
+const startPieces = 3;
+const finalLevel = 18;
---
+++ main.js
@@ -0,0 +1,216 @@
+var c;
+var ctx;
+var ost;
+var flip;
+
+var message;
+var stopped;
+var level;
+var pieces;
+var squares;
+var squareCount;
+var squarePower;
+
+var pX;
+var pY;
+var msgPad;
+
+function resizeCanvas () {
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
+}
+
+function onMouseDown (e) {
+	if (message.length > 0) {
+		if (stopped) prepareStartLevel();
+		init();
+		return;
+	}
+	
+	if (tTime == 0) {
+		for (i = 0; i < squarePower; i++) {
+			if (e.layerX >= squares[i].X && e.layerX < squares[i].X + squareWidth && e.layerY >= squares[i].Y && e.layerY < squares[i].Y + squareHeight) {
+				if (squares[i].Played) {
+					if (!squares[i].Clicked) playSound();
+					squares[i].Clicked = true;
+					if (numberOfClicked() == pieces) {
+						squares[i].X = pX + squareWidth * Math.floor(i / squareCount);
+						squares[i].Y = pY + squareHeight * (i % squareCount);
+						
+						fillRect(squares[i].X, squares[i].Y, squareWidth, squareHeight, remainColor);
+						
+						timerTick();
+						level++;
+						pieces++;
+						// Add more squares when more than half squares are played
+						if (pieces > Math.floor(squarePower / 2)) {
+							if (squareCount == Math.sqrt(bound)) prepareStartLevel();
+							else {
+								pieces = startPieces;	// reset the number of played squares, make the game easier
+								setCount(squareCount + 1);
+							}
+						}
+						
+						if (level > finalLevel) {
+							stopped = true;
+							message = "CONGRATULATION FOR FINISHING THE GAME! Click to play again.";
+						}
+						else {
+							stopped = false;
+							message = "YOU WIN. Click to continue.";
+						}
+						drawMessage(message, pX, pY - (45 + msgPad));
+					}
+				}
+				else {
+					timerTick();
+					stopped = true;
+					message = "YOU LOSE! Click to play again.";
+					drawMessage(message, pX, pY - (45 + msgPad));
+					for (j = 0; j < squarePower; j++) {
+						if (squares[j].Played && !squares[j].Clicked) {
+							squares[j].X = pX + squareWidth * Math.floor(j / squareCount);
+							squares[j].Y = pY + squareHeight * (j % squareCount);
+							
+							fillRect(squares[j].X, squares[j].Y, squareWidth, squareHeight, remainColor);
+							drawRect(squares[j].X, squares[j].Y, squareWidth, squareHeight);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+function playSound () {
+	flip.currentTime = 0;
+	flip.play();
+}
+
+function fillRect (x, y, w, h, s) {
+	ctx.fillStyle = s == null ? "#fff" : s;
+	ctx.fillRect(x, y, w, h);
+}
+
+function drawRect (x, y, w, h, s) {
+	ctx.strokeStyle = (s == null) ? "#000" : s;
+	ctx.strokeRect(x, y, w, h);
+	ctx.stroke();
+}
+
+function drawMessage (msg, x, y) {
+	ctx.font = msgFont;
+	ctx.fillStyle = msgTextColor;
+	ctx.fillText(msg, x, y + 12);
+}
+
+function timerTick () {
+	if (message.length == 0) {
+		if (tTime > 0) tTime--;
+		
+		// Invalidate
+		c.width = c.width;
+		
+		// Prepare paddings
+		pX = (c.width - (squareWidth * squareCount)) / 2;
+		pY = (c.height - (squareHeight * squareCount)) / 2;
+		msgPad = c.width / (msgFontSize * 6);
+		
+		// Draw messages
+		drawMessage("Level " + level, pX, pY - ((msgFontSize * 5) + msgPad));
+		if (tTime > 0) drawMessage("REMEMBER!", pX, pY - (msgFontSize + msgPad));
+		else drawMessage("PLAY!", pX, pY - (msgFontSize + msgPad));
+		
+		// Draw squares
+		for (i = 0; i < squarePower; i++) {
+			squares[i].X = pX + squareWidth * Math.floor(i / squareCount);
+			squares[i].Y = pY + squareHeight * (i % squareCount);
+			
+			if (tTime > 0) fillRect(squares[i].X, squares[i].Y, squareWidth, squareHeight, squares[i].Played ? squareColor : "#fff");
+			else fillRect(squares[i].X, squares[i].Y, squareWidth, squareHeight, squares[i].Clicked ? squareColor : "#fff");
+			drawRect(squares[i].X, squares[i].Y, squareWidth, squareHeight);
+		}
+	}
+}
+
+function setCount (x) {
+	squareCount = x;
+	squarePower = x * x;
+}
+
+function numberOfPlayed () {
+	var n = 0;
+	for (i = 0; i < squarePower; i++) if (squares[i].Played) n++;
+	return n;
+}
+
+function numberOfClicked () {
+	var n = 0;
+	for (i = 0; i < squarePower; i++) if (squares[i].Clicked) n++;
+	return n;
+}
+
+function prepareStartLevel() {
+	level = 1;
+	pieces = startPieces;
+	setCount(startSquares);
+	ost.play();
+}
+
+function init () {
+	// Initialize squares
+	for (i = 0; i < squarePower; i++) squares[i].Played = squares[i].Clicked = false;
+	
+	// Randomly pick played squares
+	while (numberOfPlayed() < pieces) {
+		indx = Math.floor(Math.random() * squarePower);
+		if (!squares[indx].Played) squares[indx].Played = true;
+	}
+	
+	stopped = false;
+	message = "";
+	
+	tTime = trialTime;
+	playSound();
+}
+
+window.onload = function () {
+	window.onresize = resizeCanvas;
+	
+	// Prepare body
+	document.body.style.background = bodyBackColor;
+	document.body.style.color = bodyTextColor;
+	document.body.style.font = bodyFont;
+	
+	// Prepare canvas
+	c = document.getElementById("myCanvas");
+	c.style.background = canvasBackColor;
+	c.style.position = canvasPosition;
+	resizeCanvas();
+	c.style.left = (window.innerWidth - c.width) / 2;
+	c.style.top = (window.innerHeight - c.height) / 2;
+	c.onmousedown = onMouseDown;
+	ctx = c.getContext("2d");
+	
+	// Prepare audio
+	ost = document.getElementById("myAudio");
+	ost.style.visibility = audioVisibility;
+	ost.addEventListener('ended', function() {
+		this.currentTime = 0;
+		this.play();
+	}, false);
+	
+	// Prepare sound effect
+	flip = document.getElementById("mySound");
+	flip.style.visibility = audioVisibility;
+	
+	// Prepare squares
+	squares = new Array(bound);
+	for (i = 0; i < bound; i++) squares[i] = new Object;
+	
+	message = "CLICK TO START PLAYING";
+	drawMessage(message, (c.width - (message.length * 8)) / 2, (c.height - msgFontSize) / 2);
+	stopped = true;
+	
+	setInterval(timerTick, interval);
+}
