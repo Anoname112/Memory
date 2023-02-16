c5bae3968fbdc9d29558dc0ca55a8169502d1478
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-02-16 15:36:42 +0700

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,3 +1,6 @@
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+const isPortrait = window.innerWidth < window.innerHeight;
+
 const bodyBackColor = "#fff";
 const bodyTextColor = "#fff";
 const bodyFont = "15px Segoe UI";
@@ -5,12 +8,12 @@ const canvasBackColor = "#fff";
 const canvasPosition = "fixed";
 const audioVisibility = "hidden";
 const msgTextColor = "#000";
-const msgFontSize = 15;
+const msgFontSize = isMobile ? 20 : 15;
 const msgFont = msgFontSize + "px Consolas";
 
 const interval = 10;
 
-const squareWidth = (window.innerWidth + window.innerHeight) / 40;
+const squareWidth = isMobile ? (window.innerWidth + window.innerHeight) / 25 : (window.innerWidth + window.innerHeight) / 40;
 const squareHeight = squareWidth;
 const squareColor = "#f4a460";
 const remainColor = "#a4ff80";
