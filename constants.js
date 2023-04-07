0766b240b7c32b95f3272f45150afc2f582cc7c1
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-04-07 14:42:01 +0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -4,8 +4,8 @@ const isPortrait = window.innerWidth < window.innerHeight;
 const interval = 10;
 const squareWidth = isMobile ? (window.innerWidth + window.innerHeight) / 25 : (window.innerWidth + window.innerHeight) / 40;
 const squareHeight = squareWidth;
-const squareColor = "#f4a460";
-const remainColor = "#a4ff80";
+const squareColor = "#F4A460";
+const remainColor = "#A4FF80";
 const rememberTime = 150;
 const bound = 49;		// Must be sqrt-able, for example: 9, 16, 25, 36, 49, 64, 81, 100, 121, ...
 const startSquares = 3;
@@ -14,18 +14,18 @@ const finalLevel = 18;
 
 // Body
 const bodyMargin = "0";
-const bodyBackColor = "#fff";
-const bodyTextColor = "#fff";
+const bodyBackColor = "#FFFFFF";
+const bodyTextColor = "#FFFFFF";
 const bodyFont = "15px Segoe UI";
 
 // Canvas
-const canvasBackColor = "#fff";
+const canvasBackColor = "#FFFFFF";
 const canvasPosition = "fixed";
 
 // Sound
 const audioVisibility = "hidden";
 
 // Message
-const msgTextColor = "#000";
+const msgTextColor = "#000000";
 const msgFontSize = isMobile ? 20 : 15;
 const msgFont = msgFontSize + "px Consolas";
